9d2a107fdc8648cad9bbca4bcc02bc53f03f9ef8
ankurdotio <prajapatiankur8349@gmail.com>
2026-04-30 21:45:36 +0530

    feat: initialize frontend with React and Vite
    - Added package.json for frontend dependencies and scripts
    - Created favicon.svg for the application
    - Added icons.svg containing various SVG icons
    - Implemented App.css for styling the application
    - Developed App.jsx component to fetch and display users
    - Set up main.jsx as the entry point for the React application
    - Configured Vite with a proxy for API requests to the backend

---
+++ docker-compose/frontend/vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [ react() ],
+  server: {
+    host: "0.0.0.0",
+    watch: {
+      usePolling: true,
+    },
+    proxy: {
+      '/api': {
+        target: 'http://backend:3000',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
+})
